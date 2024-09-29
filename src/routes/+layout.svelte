9af76778e8763b934d96fc7b8f266a9d1f5fdf4c
SergioD <sergio_david2109@hotmail.com>
2024-09-29 12:38:17 -0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+</script>
+
+<section class="w-full h-full flex flex-col">
+	<header class="w-fit self-center h-14 flex items-center">
+		<ul class="flex gap-x-10">
+			<li class="hover:underline"><a href="/#">About me</a></li>
+			<li class="hover:underline"><a href="/#">Projects</a></li>
+			<li class="hover:underline"><a href="/#">Contact me</a></li>
+		</ul>
+	</header>
+	<main>
+		<slot />
+	</main>
+</section>
